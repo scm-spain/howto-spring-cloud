53489085be0676b981261445eb1e6c12528a2d3b
José Armesto <github@armesto.net>
2016-05-10 17:20:42 +0200

    Gradle task to run integration tests

--- jdbc/integration-tests.sh
+++ jdbc/integration-tests.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-./gradlew clean build
 docker-compose -f docker-compose-integration.yml up -d
-docker-compose -f docker-compose-integration.yml run --rm app gradle integrationTests
+docker-compose -f docker-compose-integration.yml run --rm app gradle dockerIntegrationTests
+exit=$?
 docker-compose -f docker-compose-integration.yml down
+exit ${exit}
